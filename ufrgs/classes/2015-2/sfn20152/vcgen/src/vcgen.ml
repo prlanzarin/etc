ad0b91ba348088b1d4de305af29fccb26913d7de
prlanzarin <prlanzarin@inf.ufrgs.br>
2015-12-07 07:42:00 -0200

    vcgen done

--- ufrgs/classes/2015-2/sfn20152/vcgen/src/vcgen.ml
+++ ufrgs/classes/2015-2/sfn20152/vcgen/src/vcgen.ml
@@ -1,3 +1,13 @@
+(* ************************************************
+ *
+ * PROFESSOR: ÁLVARO MOREIRA
+ * DISCIPLINA: SEMÂNTICA FORMAL N - 2015/2
+ * TRABALHO 3: GERADOR DE CONDIÇÕES DE VERIFICAÇÃO
+ * ALUNO: PAULO RENATO LANZARIN
+ * CARTÃO: 228818
+ *
+ * ************************************************)
+
 type status = Progr | Logical
 
 type aexp = 
@@ -38,7 +48,7 @@ let rec aexpTostr (a:aexp) = match a with
     Var(x, _) -> x
     | Num(n) -> string_of_int n
     | Sum(a1, a2) -> "(" ^ (aexpTostr a1) ^ " + " ^ (aexpTostr a2) ^ ")"
-    | Mult(a1, a2) -> "(" ^ (aexpTostr a1) ^ " x " ^ (aexpTostr a2) ^ ")"
+    | Mult(a1, a2) -> "(" ^ (aexpTostr a1) ^ " * " ^ (aexpTostr a2) ^ ")"
     | Min(a1, a2) -> "(" ^ (aexpTostr a1) ^ "   " ^ (aexpTostr a2) ^ ")"
     | Fat(a1) -> "(" ^ (aexpTostr a1) ^ "!)"
 
@@ -85,25 +95,119 @@ let rec subst x (a:aexp) = function
     | AUneq(t1, t2) -> AUneq(asubst x a t1, asubst x a t2)
 
 (* WPC generator function *)
-let rec wpc (c:cmd) (psi:astn) = match c with 
+let rec wpc (c, psi) = match c with 
     Skip -> psi
     | Asg(s, ae) -> subst s ae psi
-    | Seq(c1, c2) -> wpc c1 (wpc c2 psi)
-    | If(be, c1, c2) -> AAnd(AImpl(bexpToastn be, (wpc c1 psi)), 
-                                AImpl((ANot (bexpToastn be)), wpc c2 psi))
+    | Seq(c1, c2) -> wpc (c1, (wpc(c2, psi)))
+    | If(be, c1, c2) -> AAnd(AImpl(bexpToastn be, (wpc(c1, psi))), 
+                                AImpl((ANot (bexpToastn be)), wpc(c2, psi)))
     | Wh(be, inv, c) -> inv
 
 (* auxiliary vcg function *)
-let rec vcg (c:cmd) (psi:astn) = match c with
+let rec vcg (c, psi) = match c with
     | Skip -> ABool (true)
     | Asg(s, ae) -> ABool (true)
-    | Seq(c1, c2) -> AAnd (vcg c1 (wpc c2 psi), vcg c2 psi)
-    | If(be, c1, c2) -> AAnd(vcg c1 psi, vcg c2 psi)
+    | Seq(c1, c2) -> AAnd (vcg(c1, (wpc(c2, psi))), vcg(c2, psi))
+    | If(be, c1, c2) -> AAnd(vcg(c1, psi), vcg(c2,psi))
     | Wh(be, inv, c) -> AAnd(
-                            AAnd(vcg c inv, AImpl(AAnd(inv, (bexpToastn be)),
-                                                    wpc c inv)),
+                            AAnd(vcg(c,inv), AImpl(AAnd(inv, (bexpToastn be)),
+                                                    wpc(c,inv))),
                             AImpl(AAnd(inv, ANot (bexpToastn be)), psi)) 
 
-let vcgen (psi:astn) (c:cmd) (phi:astn) = 
-    AAnd(AImpl(psi, (wpc c phi)), vcg c phi);;
+(* vcgen (astn * cmd * astn) -> astn *)
+let vcgen (psi, c, phi) = 
+    print_string (astnTostr (AAnd(AImpl(psi, (wpc(c,phi))), vcg(c,phi))));
+    print_string "\n\n" ;;
+
+(* fat function as defined in the specification *)
+let fat =
+    Seq(Seq(Asg("y", Num 1), Asg("z" , Num 0)) ,
+        Wh(PUneq(Var("z", Progr) , Var("x", Progr)),
+            AEq(Var("y" , Progr) , Fat(Var("z", Progr))),
+            Seq (Asg("z", Sum(Var("z", Progr), Num 1)),
+                Asg ("y", Mult(Var("y", Progr), Var("z", Progr))))));;
+
+(* fat2 function as defined in the exercises list *)
+let fat2 = 
+    Seq(Asg("y", Num 1),
+        Wh(PUneq(Var("x", Progr), Num 0),
+            AEq(Var("y", Progr), Fat(Var("z", Progr))),
+            Seq(Asg("y", Mult(Var("y", Progr), Var("x", Progr))),
+                Asg("x", Sum(Var("x", Progr), Num (-1))))));;
+
+
+(* vcgen for fat app as defined in the specification *)
+print_string "VCGEN DE {x >= 0} fat {y = x!}\n";;
+
+vcgen(ALeq (Num 0, Var("x" , Progr)),
+        fat,
+        AEq(Var("y", Progr), Fat(Var("x", Progr))));; 
+
+(* vcgen for fat with pre-condition {T} and post-condition {y = x!}
+ * as defined in the exercises list *)
+print_string "VCGEN DE {T} fat {y = x!}\n";;
+
+vcgen(ABool(true),
+        fat,
+        AEq(Var("y", Progr), Fat(Var("x", Progr))));; 
+
+
+(* vcgen for fat2 with pre-condition{x = x0 ^ x >= 0} and post-condition {y = x0!}
+ * as defined in the exercises list *)
+print_string "VCGEN DE {x = x0 and x >= 0} fat2 {y = x!}\n";;
+
+vcgen(AAnd(AEq(Var("x0", Progr), Var("x", Progr)), ALeq(Num 0, Var("x", Progr))),
+        fat2,
+        AEq(Var("y", Progr), Fat(Var("x0", Progr))));; 
+
+
+(* SUCC *)
+
+let succ = 
+    Seq(Asg("a", (Sum(Var("x", Progr), Num 1))),
+        If((PEq(Sum(Var("a", Progr), Num (-1)), Num 0)), 
+            (Asg("y", Num 1)),
+            (Asg("y", Var("a", Progr)))));;
+
+(* vcgen for succ with pre-condition {T} and post-condition {y = x + 1}
+ * as defined in the exercises list *)
+
+print_string "VCGEN DE {T} succ {y = x + 1}\n";;
+vcgen(ABool(true),
+        succ,
+        (AEq(Var("y", Progr), Sum(Var("x", Progr), Num 1))));;
+
+
+(* EX2 MIN *)
+let p =
+    If((PEq(Var("y", Progr), Var("x", Progr))),
+        (Asg("z", Var("y", Progr))),
+        (Asg("z", Var("x", Progr))));;
+
+
+(* vcgen for p with pre-condition {T} and post-condition {Min(x, y)}
+ * as defined in the exercises list *)
+
+print_string "VCGEN DE {T} p {z = Min(x, y)}\n";;
+
+vcgen(ABool(true),
+        p,
+        AEq(Var("z", Progr), Min(Var("x", Progr), Var("y", Progr))));;
+
+(* EX COPY1, INV = y+a = x *)
+let copy1 = 
+    Seq((Seq(Asg("a", Var("x", Progr)),
+                 Asg("y", Num 0))),
+            Wh(PUneq(Var("a", Progr), Num 0),
+                AEq(Var("x", Progr), Sum(Var("y", Progr), Var("a", Progr))),
+                Seq(Asg("y", Sum(Var("y", Progr), Num 1)),
+                        Asg("a", Sum(Var("a", Progr), Num (-1))))));;           
+
+(* vcgen for copy1 with pre-condition {x >= 0} and post-condition {x = y}
+ * as defined in the exercises list *)
+
+print_string "VCGEN DE {x >= 0} copy1 {x = y}\n";;
+vcgen(ALeq(Num 0, Var("x", Progr)),
+        copy1,
+        AEq(Var("x", Progr), Var("y", Progr)));;
 
